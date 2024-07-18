bd9b80bdc5497c2aaf902dc9e6ef1f045f7da715
Ayush-dvplr <ayush.dvlpr@gmail.com>
2024-07-18 21:54:34 +0530

    added opening instagram home page and filling username and pass , also clicking submit button

--- index.js
+++ index.js
@@ -8,24 +8,14 @@ const browser = await puppeteer.launch({
 const page = await browser.newPage();
 
 // Navigate the page to a URL.
-await page.goto("https://www.instagram.com/");
-
+await page.goto("https://www.instagram.com/", {
+  waitUntil: "networkidle2",
+});
 // Set screen size.
 await page.setViewport({ width: 1080, height: 1024 });
+await page.locator("input[name='username']").fill("Ayush");
+await page.locator("input[name='password']").fill("pasdvavsas");
 
-// // Type into search box.
-// await page.locator(".devsite-search-field").fill("automate beyond recorder");
-
-// // Wait and click on first result.
-// await page.locator(".devsite-result-item-link").click();
-
-// // Locate the full title with a unique string.
-// const textSelector = await page
-//   .locator("text/Customize and automate")
-//   .waitHandle();
-// const fullTitle = await textSelector?.evaluate((el) => el.textContent);
-
-// // Print the full title.
-// console.log('The title of this blog post is "%s".', fullTitle);
+await page.locator("button[type='submit']").click();
 
 // await browser.close();
